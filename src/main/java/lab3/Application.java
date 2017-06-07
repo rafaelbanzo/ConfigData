55ede53d14f6c8c81e7645475225c8945d7e3f9b
rlbo <rlbo@gft.com>
2017-06-07 13:53:30 +0200

    first commit

---
+++ src/main/java/lab3/Application.java
@@ -0,0 +1,20 @@
+package lab3;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Application {
+
+    /**
+     * Used when run as a JAR
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
